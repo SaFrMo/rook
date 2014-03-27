a0b48329aa0114264b60714f694a4eeef9d95add
tbc12 <chr07012@byui.edu>
2014-03-27 14:05:43 -0600

    Everything that was in the dropbox markwater level

---
+++ Assets/DGS Assets/Scripts/NPCs/castleGuard/CastleGuardMotion.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CastleGuardMotion : MonoBehaviour {
+
+	static string walkingAnimation = "walk_40";
+
+	// ANIMATION STATES FOR CASTLE GUARD
+	public bool IsMoving { get; private set; }
+	// bool IsWalking;
+
+	Vector3 lastPosition;
+
+	void Animate() {
+		if (IsMoving) {
+
+
+
+			// if (isWalking) {
+			if (!animation.IsPlaying (walkingAnimation)) {
+				animation.Play (walkingAnimation);
+			}
+		}
+	}
+
+	void Update () {
+		if (transform.position != lastPosition) {
+			IsMoving = true;
+		}
+		lastPosition = transform.position;
+
+		Animate ();
+	}
+
+}
---
+++ Assets/DGS Assets/Scripts/rook/motion/CombatRook.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatRook : MonoBehaviour {
+
+	string attackAnimation = "Attack";
+	Animation a;
+	bool doneAttacking = true;
+
+	void Attack() {
+		animation.Play (attackAnimation);
+	}
+
+	void Update () {
+		// THIS IS THE ATTACK TRIGGER
+		if (Input.GetMouseButton(0)) {
+			Attack ();
+		}
+
+
+
+	}
+}
